284474ecce786ea4efd161ac26c37e19fac75f7b
Caio Oliveira <caioaao@gmail.com>
2020-08-14 19:05:02 -0400

    finish reorg

--- notebooks/constants.py
+++ notebooks/constants.py
@@ -6,3 +6,9 @@ RAW_DATA_DIR = os.path.join(DATA_DIR, '01-raw')
 PROCESSED_DATA_DIR = os.path.join(DATA_DIR, '02-processed')
 MODELS_DIR = os.path.join(DATA_DIR, '03-models')
 MODEL_OUTPUTS_DIR = os.path.join(DATA_DIR, '04-model-output')
+
+# also load them as env variables
+os.environ["RAW_DATA_DIR"] = RAW_DATA_DIR
+os.environ["PROCESSED_DATA_DIR"] = PROCESSED_DATA_DIR
+os.environ["MODELS_DIR"] = MODELS_DIR
+os.environ["MODEL_OUTPUTS_DIR"] = MODEL_OUTPUTS_DIR
