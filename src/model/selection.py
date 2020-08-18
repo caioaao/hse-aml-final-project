daeca986be856a6f3a910568318eb5e8315223f4
Caio Oliveira <caioaao@gmail.com>
2020-08-18 09:01:08 -0400

    validation pipeline notebook

--- src/model/selection.py
+++
@@ -1,5 +0,0 @@
-from sklearn.model_selection import cross_validate
-
-
-def _cross_validate():
-    cross_validate()
--- src/model/tscv.py
+++ src/model/tscv.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 
-def split(X, n=3, date_col=0, window=None):
-    dates = X[:, date_col]
-    return [(np.where((dates >= (0 if not window else k - window - 1))
-                      & (dates < k))[0],
-             np.where(dates == k)[0])
+def split(date_vec, n=3, window=None):
+    return [(np.where((date_vec >= (0 if not window else k - window - 1))
+                      & (date_vec < k))[0],
+             np.where(date_vec == k)[0])
             for k in range(33 - n + 1, 34)]
