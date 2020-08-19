b0b5823ceafb0caf310de64aafce1113680cf87c
Caio Oliveira <caioaao@gmail.com>
2020-08-18 22:08:05 -0400

    updated all feature engineering to use clippedoutputregressor

---
+++ src/model/__init__.py
@@ -0,0 +1,21 @@
+from sklearn.base import (BaseEstimator, MetaEstimatorMixin, RegressorMixin,
+                          clone)
+import numpy as np
+
+
+class ClippedOutputRegressor(BaseEstimator, MetaEstimatorMixin,
+                             RegressorMixin):
+    def __init__(self, regressor):
+        self.regressor = regressor
+
+    def fit(self, *args, **kwargs):
+        self.regressor_ = clone(self.regressor)
+        self.regressor_.fit(*args, **kwargs)
+        return self
+
+    def predict(self, *args, **kwargs):
+        return np.clip(self.regressor_.predict(*args, **kwargs), 0, 20)
+
+    def fit_predict(self, *args, **kwargs):
+        self.regressor_ = clone(self.regressor)
+        return np.clip(self.regressor_.fit_predict(*args, **kwargs), 0, 20)
