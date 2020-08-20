f66bb419789b2edec1ac4020ab8c70543cc857a7
Caio Oliveira <caioaao@gmail.com>
2020-08-20 16:53:42 -0400

    do not reduce dataset prematurely

--- src/data/make_train_set.py
+++ src/data/make_train_set.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
     sales_train_by_month['item_cnt_month'] = \
         np.clip(sales_train_by_month['item_cnt'], 0, 20)
 
-    sales_train_by_month.loc[
-        sales_train_by_month['date_block_num'] > 20,
-        ['date_block_num', 'item_id', 'shop_id', 'item_cnt_month']]\
+    sales_train_by_month[['date_block_num', 'item_id', 'shop_id',
+                          'item_cnt_month']]\
         .to_parquet(output_path)
