d742f45852b3db4e4929dcfbc90d785842400684
Caio Oliveira <caioaao@gmail.com>
2020-08-20 16:57:05 -0400

    more parameters to tscv

--- src/model/tscv.py
+++ src/model/tscv.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 
-def split(date_vec, n=3, window=None):
+def split(date_vec, n=3, window=None, test_months=None):
+    test_months = test_months if test_months is not None else range(1, 34)
+    if n is not None:
+        test_months = test_months[-n:]
     return [(np.where((date_vec >= (0 if not window else k - window - 1))
                       & (date_vec < k))[0],
              np.where(date_vec == k)[0])
-            for k in range(33 - n + 1, 34)]
+            for k in test_months]
