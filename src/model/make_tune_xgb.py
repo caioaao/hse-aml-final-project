9e3e85c002082b8e5bdc044946bb732089acc202
Caio Oliveira <caioaao@gmail.com>
2020-08-30 19:43:43 -0400

    xgb improvement + install lgb

--- src/model/make_tune_xgb.py
+++ src/model/make_tune_xgb.py
@@ -2,14 +2,118 @@ import joblib
 
 import pandas as pd
 import optuna
+from optuna.integration import XGBoostPruningCallback
 
-from .xgb import (make_xgb_loss, make_xgb_objective, best_num_round,
-                  sklearn_regressor)
+import numpy as np
+import xgboost as xgb
+from xgboost import XGBRegressor
+from optuna import Trial
+from sklearn.metrics import mean_squared_error
+
+
 from . import tscv
 from ..feature_engineering import df_to_X_y
 
 
 MAX_EVALS = 200
+
+
+DEFAULT_PARAMS = {"n_jobs": -1}
+
+
+def _xgb_feval(y_pred, dtrain):
+    try:
+        result = mean_squared_error(
+            dtrain.get_label(), np.clip(y_pred, 0, 20),
+            squared=False)
+    except ValueError:
+        result = np.nan
+    return 'clipped-rmse', result
+
+
+def _trial_to_params(trial: Trial):
+    params = {**DEFAULT_PARAMS,
+              "booster": trial.suggest_categorical(
+                  "booster", ['gbtree', 'gblinear']),
+              "objective": trial.suggest_categorical(
+                  "objective", ["reg:squarederror", "reg:gamma"]),
+              "seed": trial.suggest_int('seed', 0, 999999),
+              "learning_rate": trial.suggest_loguniform(
+                  'learning_rate', 0.005, 0.5),
+              "reg_alpha": trial.suggest_categorical(
+                  "reg_alpha", [0, 0, 0, 0, 0, 0.00000001,
+                                0.00000005, 0.0000005, 0.000005]),
+              "reg_lambda": trial.suggest_categorical(
+                  "reg_lambda", [1, 1, 1, 1, 2, 3, 4, 5, 1])}
+
+    if params['booster'] == 'gbtree':
+        params.update({
+            "max_depth": trial.suggest_int('max_depth', 2, 15, 1),
+            "subsample": trial.suggest_discrete_uniform('subsample',
+                                                        .2, 1, .05),
+            "colsample_bytree": trial.suggest_discrete_uniform(
+                'colsample_bytree', .20, 1., .01),
+            "colsample_bylevel": trial.suggest_discrete_uniform(
+                'colsample_bylevel', .20, 1., .01),
+            "colsample_bynode": trial.suggest_discrete_uniform(
+                'colsample_bynode', .20, 1., .01),
+            "gamma": trial.suggest_categorical("gamma", [0, 0, 0, 0, 0, 0.01,
+                                                         0.1, 0.2, 0.3, 0.5,
+                                                         1., 10., 100.]),
+            "min_child_weight": trial.suggest_categorical('min_child_weight',
+                                                          [1, 1, 1, 1, 2, 3,
+                                                           4, 5, 1, 6, 7, 8, 9,
+                                                           10, 11, 15, 30, 60,
+                                                           100, 1, 1, 1]),
+            "max_delta_step": trial.suggest_categorical("max_delta_step",
+                                                        [0, 0, 0, 0, 0,
+                                                         1, 2, 5, 8]),
+            "tree_method": "gpu_hist",
+            "gpu_id": 0})
+    return params
+
+
+def make_xgb_loss(X_train, y_train, cv_splits, verbose=True):
+    dtrain = xgb.DMatrix(X_train, y_train)
+
+    def loss(params, callbacks=[]):
+        return xgb.cv(
+            params, dtrain,
+            callbacks=callbacks,
+            folds=cv_splits, verbose_eval=verbose,
+            feval=_xgb_feval, maximize=False, num_boost_round=1000,
+            early_stopping_rounds=10
+        )['test-clipped-rmse-mean'].min()
+
+    return loss
+
+
+def make_xgb_objective(xgb_loss):
+    return lambda trial: xgb_loss(
+        _trial_to_params(trial), callbacks=[
+            XGBoostPruningCallback(
+                trial, observation_key='test-clipped-rmse')])
+
+
+def train(params, X_train, y_train):
+    dtrain = xgb.DMatrix(X_train, y_train)
+    return xgb.train({**DEFAULT_PARAMS, **params}, dtrain)
+
+
+def best_num_round(params, X, y, cv_splits, verbose=True):
+    params = {**DEFAULT_PARAMS, **params}
+    train_idx, test_idx = cv_splits[-1]
+    dtrain = xgb.DMatrix(X[train_idx], y[train_idx])
+    dtest = xgb.DMatrix(X[test_idx], y[test_idx])
+    bst = xgb.train(params, dtrain, early_stopping_rounds=50,
+                    num_boost_round=1000, evals=[(dtrain, 'dtrain'),
+                                                 (dtest, 'dtest')],
+                    feval=_xgb_feval, verbose_eval=verbose)
+    return bst.best_ntree_limit
+
+
+def sklearn_regressor(params, num_round):
+    return XGBRegressor(n_estimators=num_round, **DEFAULT_PARAMS, **params)
 
 
 if __name__ == '__main__':
@@ -31,7 +135,7 @@ if __name__ == '__main__':
         pruner=optuna.pruners.HyperbandPruner())
 
     try:
-        study.optimize(objective, n_trials=50, n_jobs=2, gc_after_trial=True)
+        study.optimize(objective, n_trials=50, n_jobs=1, gc_after_trial=True)
     except KeyboardInterrupt:
         print("Canceling optimization step before it finishes")
 
