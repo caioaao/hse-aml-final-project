124b0eb5a28538a5b5cc32b397dc7c20c2550819
Caio Oliveira <caioaao@gmail.com>
2020-09-09 10:23:41 -0400

    fix feature set 009 and add clipped output to baseline

--- src/feature_engineering/make_feature_set_009.py
+++ src/feature_engineering/make_feature_set_009.py
@@ -8,7 +8,6 @@ FEATURE_COLS = ['item_shop_price_median',
                 'item_price_median',
                 'cat_price_median']
 # copied from 007
-MONTHS_TO_DROP = [5, 7, 8, 9, 11, 17, 19, 21, 22, 23, 25, 27, 29]
 
 
 if __name__ == '__main__':
@@ -18,12 +17,7 @@ if __name__ == '__main__':
     output_path = sys.argv[4]
 
     df = input_df.merge(categories_meta, on='item_id')
-    df = add_lagged_features(df, stats_df, FEATURE_COLS, fill_value=-999)
-
-    df.drop(columns=['f__%s_%d' % (col, i)
-                     for i in MONTHS TO DROP
-                     for col in FEATURE_COLS],
-            inplace=True)
+    df = add_lagged_features(df, stats_df, FEATURE_COLS, fill_value=-999, max_lag=6)
 
     print("%s columns: %s" % (output_path, str(df.columns)))
     df.to_parquet(output_path)
--- src/model/make_xgb_baseline.py
+++ src/model/make_xgb_baseline.py
@@ -1,9 +1,10 @@
 import sys
 import joblib
 import xgboost as xgb
+from . import ClippedOutputRegressor
 
 if __name__ == '__main__':
     output_path = sys.argv[1]
     reg = xgb.XGBRegressor(random_state=13, tree_method='gpu_hist',
-                           gpu_id=0, n_jobs=-1)
-    joblib.dump(reg, output_path)
+                           gpu_id=0, n_jobs=-1, missing=-999)
+    joblib.dump(ClippedOutputRegressor(reg), output_path)
