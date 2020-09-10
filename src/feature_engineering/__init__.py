8e24c47c8db2fee6da7f275f44f11f2d49111968
Caio Oliveira <caioaao@gmail.com>
2020-09-10 10:36:18 -0400

    oops

--- src/feature_engineering/__init__.py
+++ src/feature_engineering/__init__.py
@@ -21,7 +21,7 @@ def add_lagged_features(
         df = df.merge(tmp_df[['date_block_num'] + lag_cols + index_cols],
                       on=index_cols + ['date_block_num'], how='left',
                       sort=False)
-    if fill_value:
+    if fill_value is not None:
         df.fillna(fill_value, inplace=True)
     return df
 
