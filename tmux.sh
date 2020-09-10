00ce1a05a4a5d87fa75774079f1e16e8922de46a
Caio Oliveira <caioaao@gmail.com>
2020-09-10 13:51:56 -0400

    update baseline

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tmux new-session -d 'pipenv run jupyter lab'
+tmux new-session -d
 tmux split-window -v 'vtop'
 tmux split-window -h 'watch -d -n 1 nvidia-smi'
 tmux -2 attach-session -d
