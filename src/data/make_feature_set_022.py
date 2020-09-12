e0f32db67542775497360fcb9edaff296a62ec1e
Caio Oliveira <caioaao@gmail.com>
2020-09-12 11:42:10 -0400

    fix feature set 022

--- src/data/make_feature_set_022.py
+++ src/data/make_feature_set_022.py
@@ -5,12 +5,22 @@ from . import add_as_features
 
 if __name__ == '__main__':
     df = pd.read_parquet(sys.argv[1])
-    hist_df = pd.read_parquet(sys.argv[2])
+    release_dates = pd.read_parquet(sys.argv[2])
     output_path = sys.argv[3]
 
-    df = df.merge(hist_df, on=['item_id', 'shop_id', 'date_block_num'],
-                  how='left', sort=False).fillna(-999)
-    add_as_features(df, [col for col in hist_df
+    df = df.merge(release_dates, on=['item_id', 'shop_id'],
+                  how='left', sort=False).fillna(999)
+    dates_cols = [col for col in release_dates.columns
+                  if col.endswith('_release_date')]
+
+    for col in dates_cols:
+        feature_col = col.replace('_release_date', '_months_since_launch')
+        df[feature_col] = df['date_block_num'] - df[col]
+        df[df[feature_col] <= 0] = -999
+
+    df.drop(columns=dates_cols, inplace=True)  # to avoid a data leak
+
+    add_as_features(df, [col for col in df
                          if col.endswith('_since_launch')],
                     inplace=True)
 
--- src/data/make_release_dates.py
+++ src/data/make_release_dates.py
@@ -12,21 +12,17 @@ if __name__ == '__main__':
     sales_train = pd.read_parquet(sys.argv[1])
     output_path = sys.argv[2]
 
-    df = sales_train[['item_id', 'shop_id', 'date_block_num']]\
-        .drop_duplicates()
+    df = sales_train[['item_id', 'shop_id']].drop_duplicates()
 
     for group in RELEASE_DATE_GROUPS:
         group_id = '_'.join(group)
         release_date_col = '%s_release_date' % group_id
-        feature_col = '%s_months_since_launch' % group_id
         release_dates = sales_train.groupby(group)['date_block_num']\
                                    .min()\
                                    .reset_index()
         release_dates.rename(columns={'date_block_num': release_date_col},
                              inplace=True)
-        df = df.merge(release_dates, on=group, how='left').fillna(-1)
-        df[feature_col] = df['date_block_num'] - df[release_date_col]
-        df[df[feature_col] <= 0] = -999
-        df.drop(columns=[release_date_col], inplace=True)
+        df = df.merge(release_dates, on=group, how='left').fillna(999)
 
+    print(df.columns)
     df.to_parquet(output_path)
