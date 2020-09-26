6673ec5ae7b3c02cb13584c26bba44ded3ebd92f
Caio Oliveira <caioaao@gmail.com>
2020-09-26 12:25:33 -0300

    use partial fit on mlp

--- src/model/mlp.py
+++ src/model/mlp.py
@@ -29,26 +29,41 @@ class MLPRegressor(BaseEstimator, RegressorMixin, MetaEstimatorMixin):
         return self.net_(torch.from_numpy(X.todense())
                               .to('cuda', torch.float))
 
-    def fit(self, X, y):
-        self.n_features_ = self.preprocessor.transform(X[0:1, :]).todense().shape[1]
+    def _set_net(self, X):
+        self.n_features_ = self.preprocessor.transform(X[0:1, :]).todense()\
+                                                                 .shape[1]
         self.net_ = MLP(self.n_features_).to('cuda')
+        self.n_epochs_fit_ = 0
         self.optimizer_ = optim.Adam(self.net_.parameters())
         self.criterion_ = nn.MSELoss()
+
+    def partial_fit(self,  X, y):
+        if not hasattr(self, 'net_'):
+            self._set_net(X)
+
         if len(y.shape) == 1:
             y = y.reshape(-1, 1)
 
+        epoch_id = self.n_epochs_fit_ + 1
+
+        acc_loss = 0.0
+        for i in range(0, X.shape[0], self.batch_size):
+            self.optimizer_.zero_grad()
+            output = self._predict(X[i:i + self.batch_size])
+            target = torch.from_numpy(y[i:i + self.batch_size])\
+                          .to('cuda', torch.float)
+            loss = self.criterion_(output, target)
+            acc_loss = acc_loss + loss.item()
+            loss.backward()
+            self.optimizer_.step()
+        self.n_epochs_fit_ = epoch_id
+        print('Epoch %d, Acc loss: %.5f' % (epoch_id, acc_loss))
+        return self
+
+    def fit(self, X, y):
+        self._set_net(X)
         for epoch in range(self.n_epochs):
-            acc_loss = 0.0
-            for i in range(0, X.shape[0], self.batch_size):
-                self.optimizer_.zero_grad()
-                output = self._predict(X[i:i + self.batch_size])
-                target = torch.from_numpy(y[i:i + self.batch_size])\
-                              .to('cuda', torch.float)
-                loss = self.criterion_(output, target)
-                acc_loss = acc_loss + loss.item()
-                loss.backward()
-                self.optimizer_.step()
-            print('Epoch %d, Acc loss: %.5f' % (epoch, acc_loss))
+            self.partial_fit(X, y)
         return self
 
     def predict(self, X):
